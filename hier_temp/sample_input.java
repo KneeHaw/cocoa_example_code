a89ca4d23d8417e741392781293e847b6d84bfe7
KneeHaw <90938928+KneeHaw@users.noreply.github.com>
2025-05-04 20:50:24 -0500

    Create sample_input.java

---
+++ hier_temp/sample_input.java
@@ -0,0 +1,16 @@
+return returnBuffer . toString ( ) ;
+} public User createUser ( User userParam )
+int j = 0;
+for (int i = 0; i < 100; i++) {
+    System.out.println(i);
+    j = i % 2;
+}
+public class Example {
+    public static int addNumbers(int num1, int num2) {
+        int sum = num1 + num2; return sum;
+        }
+    public static void main(String[] args) {
+        int result = addNumbers(5, 3);
+        System.out.println("The sum is: " + result);
+        }
+    }
